7c7f42a8c24865a77fe60950173b61f71e1badbd
Jackson Figueiredo <jacksonsmfigueiredo@hotmail.com>
2023-03-03 10:46:53 -0300

    Add Base project.

---
+++ BasicExceptions/Entities/Reservation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicExceptions.Entities
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public 
+    }
+}
